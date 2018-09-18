d4df38f50e9f87d4821c746a806620add6e90c17
travispbuckley <travispbuckley@lewisu.edu>
2018-09-17 22:04:03 -0500

    practice object destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,11 @@
 // used to connect to the database
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// object destructuring: pull out properties from an object, creating variables
+// var user = {name: 'Travis', age: 25};
+// 'name' is the name of the new variable and the property we want to pull out.
+// var {name} = user;
 
 // first argument is url where DB lives. Amazon, heroku, localhost, etc. 
 // Second argument is callback. Fired after conenction either succeeds or fails
@@ -22,16 +28,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Travis',
-        age: 26,
-        location: 'Lisle'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert user');
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // db.collection('Users').insertOne({
+    //     name: 'Travis',
+    //     age: 26,
+    //     location: 'Lisle'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user');
+    //     }
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
 
     client.close();
 });
